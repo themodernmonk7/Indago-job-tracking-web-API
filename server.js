f6fb1bb4da7010e7fd7b544ff6749cedd3833009
themodernmonk7 <sauravavishek@gmail.com>
2022-10-25 20:04:57 +0530

    Connect to database

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require('express')
+import dotenv from "dotenv"
+dotenv.config()
+import "express-async-errors"
+import express from "express"
 const app = express()
+import connectDB from "./database/connectDB.js"
 
-app.get('/', (req, res) => {
-    res.send("<h1> Indago-job-tracking-website API </h2>")
+app.get("/", (req, res) => {
+  res.send("<h1> Indago-job-tracking-website API </h2>")
 })
 
 // Create a server
 const port = 5000 || process.env.PORT
 const start = async () => {
-    try {
-        app.listen(port, () => console.log(`🚀 Server is listening on port ${port}...`))
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`🚀 Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 start()
