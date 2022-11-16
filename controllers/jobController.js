562c6592401de38ae8a95d7c08db4a56804600d1
themodernmonk7 <sauravavishek@gmail.com>
2022-11-16 11:47:35 +0530

    Pagination setup

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -11,29 +11,41 @@ const createJob = async (req, res) => {
 
 // * === === === === ===    GET ALL JOBS    === === === === === *
 const getAllJobs = async (req, res) => {
-  const {search, status, jobType, sort} = req.query
+  const { search, status, jobType, sort } = req.query
   const queryObject = { createdBy: req.user.userId }
 
-  if(search) {
-    queryObject.position = {$regex: search, $options: 'i'}
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" }
   }
-  
-  if(status && status !== 'all') {
+
+  if (status && status !== "all") {
     queryObject.status = status
   }
 
-  if(jobType && jobType !== 'all') {
+  if (jobType && jobType !== "all") {
     queryObject.jobType = jobType
   }
   let result = Job.find(queryObject)
   // chaining sort
-  // const jobs = await Job.find({createdBy: req.user.userId}).sort('-createdAt') //latest
-  if(sort === 'latest') result = result.sort('-createdAt')
-  if(sort === 'oldest') result = result.sort('createdAt')
-  if(sort === 'a-z') result = result.sort('position')
-  if(sort === 'z-a') result = result.sort('-position')
+  // const jobs = await Job.find({createdBy: req.user.userId}).sort('-createdAt').limit(10) //latest
+  if (sort === "latest") result = result.sort("-createdAt")
+  if (sort === "oldest") result = result.sort("createdAt")
+  if (sort === "a-z") result = result.sort("position")
+  if (sort === "z-a") result = result.sort("-position")
+
+  // Setup pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const jobs = await result
-    res.status(StatusCodes.OK).json({ total_jobs: jobs.length, jobs })
+
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ totalJobs, numOfPages, jobs })
 }
 
 // * === === === === ===    GET SINGLE JOB  === === === === === *
