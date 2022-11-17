1d80ec5956c2cbc845931f2038d7b957124842ac
themodernmonk7 <sauravavishek@gmail.com>
2022-11-17 19:55:22 +0530

    Change port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 // Create a server
-const port = 5000 || process.env.PORT
+const port = process.env.PORT || 5000 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL)
