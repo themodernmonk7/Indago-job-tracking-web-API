cd8c0575c7fd16ea05e075692f9bf0c4d63d00dc
themodernmonk7 <sauravavishek@gmail.com>
2022-11-17 18:34:40 +0530

    Add Docgen API Documentation

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 app.use(fileUpload({ useTempFiles: true }))
+app.use(express.static("./public"))
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobRouter)
