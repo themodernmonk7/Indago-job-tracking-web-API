cbd32299cf76880f5c654f4eefeaac6ae00319e7
themodernmonk7 <sauravavishek@gmail.com>
2023-02-09 10:58:23 +0530

    Setup route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
   login,
   updateUser,
   uploadProfileImage,
+  emailVerification,
 } from "../controllers/authController.js"
 import testUser from "../middleware/testUser.js"
 import rateLimiter from "express-rate-limit"
@@ -19,6 +20,7 @@ const apiLimiter = rateLimiter({
 })
 
 router.route("/register").post(apiLimiter, register)
+router.route("/email-verification").post(emailVerification)
 router.route("/login").post(apiLimiter, login)
 router.route("/updateUser").patch(auth, testUser, updateUser)
 router.route("/uploadProfile").post(auth, testUser, uploadProfileImage)
