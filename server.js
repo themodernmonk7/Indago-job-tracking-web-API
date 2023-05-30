3984f2e94df5f9209867d20797c4aaf2b3e1a426
themodernmonk7 <sauravavishek@gmail.com>
2023-05-30 21:56:40 +0530

    add: origin

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.set("trust proxy", 1)
 app.use(helmet())
 app.use(
   cors({
-    origin: import.meta.env.ORIGIN,
+    origin: "https://indago-job.netlify.app",
     credentials: true, // Allow including credentials (e.g., cookies)
   })
 )
